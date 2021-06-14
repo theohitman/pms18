c2750114bf8d197b4c92e0e649b1da38dd6e28e0
Michalis Stavroulakis <77919736+itp20132@users.noreply.github.com>
2021-06-14 21:00:00 +0300

    Update quickstart_app.py

--- QuickStart-App/quickstart_app.py
+++ QuickStart-App/quickstart_app.py
@@ -88,16 +88,16 @@ def create_app():
         # User authentication information. The collation='NOCASE' is required
         # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
         username = db.Column(
-            db.String(100, collation='NOCASE'), nullable=False, unique=True)
-        email = db.Column(db.String(255, collation='NOCASE'), nullable=True)
+            db.String(100), nullable=False, unique=True)
+        email = db.Column(db.String(255), nullable=True)
         email_confirmed_at = db.Column(db.DateTime())
         password = db.Column(db.String(255), nullable=False, server_default='')
 
         # User information
         first_name = db.Column(
-            db.String(100, collation='NOCASE'), nullable=False, server_default='')
+            db.String(100), nullable=False, server_default='')
         last_name = db.Column(
-            db.String(100, collation='NOCASE'), nullable=False, server_default='')
+            db.String(100), nullable=False, server_default='')
 
         # Define the relationship to Role via UserRoles
         roles = db.relationship('Role', secondary='user_roles')
